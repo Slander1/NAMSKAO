fb1cf01fe3c62942a200f8791bf665ff60ef1863
Slander1 <ya.wexwexquas@gmail.com>
2022-08-08 03:47:21 +0300

    DragHandler moved to a separate class Refactor prefab

--- Assets/Scripts/PuzzleGeneration/PiecePazzle.cs
+++ Assets/Scripts/PuzzleGeneration/PiecePazzle.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace PuzzleGeneration
 {
-	public enum NamePos
+    public enum NamePos
 	{
 		CORNER = 0,
 		EDGE = 1,
 		CENTER = 2
 	}
 
-	public class PiecePazzle : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+	public class PiecePazzle : MonoBehaviour
 	{
 		public RectTransform elementForScroll;
+
 		public Vector3 startPos;
 		public Vector2Int posInGreed;
+
 		public bool onInitialPos = false;
+
+
+		public List<PiecePazzle> nearPieces = new List<PiecePazzle>();
 
 		[field: SerializeField] public PieceData PieceData { get; set; }
 
@@ -27,13 +31,6 @@ namespace PuzzleGeneration
 
 
 		public static Action<PiecePazzle> PiecePazzleOnInitialPos;
-
-		private Vector3 _screenPoint;
-		private Vector3 _offset;
-		private bool _inContainer = true;
-
-		private List<PiecePazzle> _nearPieces = new List<PiecePazzle>();
-		private List<PiecePazzle> _allPuzzles = new List<PiecePazzle>();
 
 
 		private void OnEnable()
@@ -46,60 +43,7 @@ namespace PuzzleGeneration
 			PiecePazzleOnInitialPos += NearPiecePazzle;
 		}
 
-		public void OnBeginDrag(PointerEventData eventData)
-		{
-			_inContainer = false;
-			_screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-			_offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x,
-				eventData.position.y, _screenPoint.z));
-		}
-
-		public void OnDrag(PointerEventData eventData)
-		{
-			var cursorPoint = new Vector3(eventData.position.x, eventData.position.y, _screenPoint.z);
-			var cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + _offset;
-			transform.position = cursorPosition;
-			if (onInitialPos)
-			{
-				var scale = transform.localScale;
-				foreach (var piece in _nearPieces)
-				{
-					piece.transform.position = cursorPosition +
-						new Vector3(3 * piece.posInGreed.x * scale.x, -3 *
-						piece.posInGreed.y * scale.y, 0f);
-				}
-			}
-		}
-
-		public void OnEndDrag(PointerEventData eventData)
-		{
-			if (onInitialPos)
-			{
-				foreach (var piece in _nearPieces)
-				{
-					piece.transform.position = piece.startPos;
-				}
-			}
-			if (onInitialPos)
-			{ 				
-				transform.position = startPos;
-				return;
-			}
-			
-			if (transform.position.x > 21.5f)
-				_inContainer = true;
-			else
-				ComparePos();
-			transform.localScale = (_inContainer) ? scaleInContainer : scaleOnBoard;
-			
-		}
-
-
-        private void Update()
-        {
-			if (_inContainer && elementForScroll)
-				transform.position = elementForScroll.position;
-		}
+        
 
 
 		private bool CheckPosInGreed(Vector2Int startPos, Vector2Int checkPos)
@@ -114,7 +58,7 @@ namespace PuzzleGeneration
 
 		public void GetListAtNearPieces(List<PiecePazzle> otherList, PiecePazzle newInNear)
         {
-            foreach (var piece in _nearPieces)
+            foreach (var piece in nearPieces)
             {
 				piece.AddNewPieceInChain(newInNear);
 				if (!otherList.Contains(piece))
@@ -126,11 +70,11 @@ namespace PuzzleGeneration
 
 		public void AddNewPieceInChain(PiecePazzle newInNear)
         {
-			if (!_nearPieces.Contains(newInNear))
-				_nearPieces.Add(newInNear);
+			if (!nearPieces.Contains(newInNear))
+				nearPieces.Add(newInNear);
         }
 
-		private void ComparePos()
+		public void ComparePos()
         {
 			if (Mathf.Abs(transform.position.x - startPos.x) < 2
 				&& Mathf.Abs(transform.position.y - startPos.y) < 2)
@@ -139,24 +83,23 @@ namespace PuzzleGeneration
 				onInitialPos = true;
 				PiecePazzleOnInitialPos?.Invoke(this);
 				var i = 0;
-				while(i < _nearPieces.Count)
+				while(i < nearPieces.Count)
 				{
-					_nearPieces[i].GetListAtNearPieces(_nearPieces, this);
+					nearPieces[i].GetListAtNearPieces(nearPieces, this);
 					i++;
 				}
 				GiveNewInfoToNear();
 				Destroy(elementForScroll.gameObject);
 			}
-
 		}
 
 		private void GiveNewInfoToNear()
         {
 			var i = 0;
-			while (i < _nearPieces.Count)
+			while (i < nearPieces.Count)
 			{
-				if (CheckPosInGreed(posInGreed, _nearPieces[i].posInGreed))
-					_nearPieces[i].GiveListToNear(_nearPieces);
+				if (CheckPosInGreed(posInGreed, nearPieces[i].posInGreed))
+					nearPieces[i].GiveListToNear(nearPieces);
 				i++;
 			}
 		}
@@ -172,12 +115,10 @@ namespace PuzzleGeneration
 		private void NearPiecePazzle(PiecePazzle otherPiecPazzle)
         {
 			if (CheckPosInGreed(posInGreed, otherPiecPazzle.posInGreed) &&
-				otherPiecPazzle!=this && (!_nearPieces.Contains(otherPiecPazzle)))
+				otherPiecPazzle!=this && (!nearPieces.Contains(otherPiecPazzle)))
 			{ 
-				_nearPieces.Add(otherPiecPazzle);
+				nearPieces.Add(otherPiecPazzle);
 			}
-			else
-				_allPuzzles.Add(otherPiecPazzle);
         }
     }
 
